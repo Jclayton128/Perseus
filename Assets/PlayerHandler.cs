55d50bf838299e9c1902503b23a1762c7a055bf9
jclayton128 <jclayton128@gmail.com>
2022-07-17 16:08:30 -0400

    Engine FX. Other Persephone things brought in.

--- Assets/PlayerHandler.cs
+++ Assets/PlayerHandler.cs
@@ -8,6 +8,7 @@ public class PlayerHandler : MonoBehaviour
     InputController _inputCon;
     Rigidbody2D _rb;
     PlayerSystemHandler _playerSystemHandler;
+    [SerializeField] ParticleSystem _playerEngineParticles = null;
 
     //settings
     float _turningForce = 300f;
@@ -79,11 +80,13 @@ public class PlayerHandler : MonoBehaviour
     {
         _isAccelerating = true;
         _isDecelerating = false;
+        _playerEngineParticles.Play();
     }
 
     private void HandleStopAccelerating()
     {
         _isAccelerating = false;
+        _playerEngineParticles.Stop();
     }
 
     private void HandleBeginDecelerating()
