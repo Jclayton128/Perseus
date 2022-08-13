b87c621255675576cb5096d85c2f3e119036d28f
jclayton128 <jclayton128@gmail.com>
2022-08-13 18:31:45 -0400

    Damage particle FX (set to hull sprites though)

--- Assets/ParticleController.cs
+++ Assets/ParticleController.cs
@@ -5,20 +5,24 @@ using UnityEngine;
 
 public class ParticleController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    [SerializeField] ParticleSystem _hullDamageFXprefab = null;
 
     internal void RequestShieldDamageParticles(int particlesToMake, Vector3 position, Vector2 impactHeading)
     {
-        throw new NotImplementedException();
+        Quaternion rot = Quaternion.LookRotation(impactHeading, Vector3.forward);
+        ParticleSystem ps = Instantiate(_hullDamageFXprefab.gameObject, position, rot).GetComponent<ParticleSystem>();
+        ParticleSystem.EmissionModule psem = ps.emission;
+
+        ParticleSystem.Burst burst = new ParticleSystem.Burst(0,particlesToMake);
+        psem.SetBurst(0, burst);
+        ps.Play();
+    }
+
+    [ContextMenu("Spawn Random hull particles")]
+    private void DebugShieldDamage()
+    {
+        RequestShieldDamageParticles(10, Vector3.one, new Vector2(0, -1));
     }
 }
